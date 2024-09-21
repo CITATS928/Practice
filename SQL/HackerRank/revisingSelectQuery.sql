5da446e9318d9ca9390bbd0e88c45bd1b6e46cb0
CITATS928 <93238522+CITATS928@users.noreply.github.com>
2024-09-20 20:54:11 -0400

    sql select query

---
+++ SQL/HackerRank/revisingSelectQuery.sql
@@ -0,0 +1,7 @@
+--Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.
+
+--https://www.hackerrank.com/challenges/revising-the-select-query/problem?isFullScreen=true
+
+SELECT * FROM CITY 
+where population > 100000
+and COUNTRYCODE = 'USA';
