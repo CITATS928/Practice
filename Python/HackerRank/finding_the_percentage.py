c30bf5536fd7deeea002d646df2c30d46073b4b8
CITATS928 <93238522+CITATS928@users.noreply.github.com>
2024-09-22 20:41:44 -0400

    percentage

--- Python/HackerRank/finding_the_percentage.py
+++ Python/HackerRank/finding_the_percentage.py
@@ -3,3 +3,22 @@ https://www.hackerrank.com/challenges/finding-the-percentage/problem?isFullScree
 
 The provided code stub will read in a dictionary containing key/value pairs of name:[marks] for a list of students. Print the average of the marks array for the student name provided, showing 2 places after the decimal.
 """
+
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    #print(student_marks)
+    sumN=0
+    for key in student_marks:
+        if key == query_name:
+            #print('name = ' + key)
+            for value in student_marks[key]:
+                sumN += value
+            result = format((sumN/3), '.2f')
+            print(result)
+
