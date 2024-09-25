0f94d907dc1a210d8fd2e64509713ac6840273f3
CITATS928 <93238522+CITATS928@users.noreply.github.com>
2024-09-25 16:21:39 -0400

    jpn countrycode

---
+++ SQL/HackerRank/japanese_city_attributes.sql
@@ -0,0 +1,6 @@
+--https://www.hackerrank.com/challenges/japanese-cities-attributes/problem?isFullScreen=true
+
+--Query all attributes of every Japanese city in the CITY table. The COUNTRYCODE for Japan is JPN.
+
+SELECT * FROM city
+WHERE countrycode = 'JPN';
