d099fcb47c4a3822dbce692461659945e921235b
CITATS928 <93238522+CITATS928@users.noreply.github.com>
2024-09-26 23:40:36 -0400

    weather obser 4

---
+++ SQL/HackerRank/weather_observation_station_4.sql
@@ -0,0 +1,10 @@
+--https://www.hackerrank.com/challenges/weather-observation-station-4/problem
+
+--Find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table.
+
+
+SELECT COUNT(city) - COUNT(DISTINCT city) FROM station;
+
+
+--note: 有些时候可能会不支持，还有性能问题和兼容性问题
+--count(distinct column)会计算null值，而count(column)不会计算null值
