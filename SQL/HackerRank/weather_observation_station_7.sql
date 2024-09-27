6e01aca9683df3027d393bd8845fde845b8d7fab
CITATS928 <93238522+CITATS928@users.noreply.github.com>
2024-09-27 17:45:50 -0400

    seather obser 7

---
+++ SQL/HackerRank/weather_observation_station_7.sql
@@ -0,0 +1,6 @@
+--https://www.hackerrank.com/challenges/weather-observation-station-7/problem
+
+--Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
+
+SELECT DISTINCT city FROM station
+WHERE RIGHT(city,1) IN ('a','e','i','o','u');
