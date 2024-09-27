1258047b5808ad8b42db6aec9c30d71f07781320
CITATS928 <93238522+CITATS928@users.noreply.github.com>
2024-09-27 00:24:14 -0400

    weather obser 4

--- SQL/HackerRank/weather_observation_station_4.sql
+++ SQL/HackerRank/weather_observation_station_4.sql
@@ -8,3 +8,9 @@ SELECT COUNT(city) - COUNT(DISTINCT city) FROM station;
 
 --note: 有些时候可能会不支持，还有性能问题和兼容性问题
 --count(distinct column)会计算null值，而count(column)不会计算null值
+
+/*
+SELECT 
+    (SELECT COUNT(city) FROM station) - 
+    (SELECT COUNT(DISTINCT city) FROM station);
+*/
