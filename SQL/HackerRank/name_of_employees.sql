a448541220eacc0f6f181e2fac08bc08ce3cb4b5
CITATS928 <93238522+CITATS928@users.noreply.github.com>
2024-09-28 17:20:12 -0400

    name of employee

---
+++ SQL/HackerRank/name_of_employees.sql
@@ -0,0 +1,6 @@
+--https://www.hackerrank.com/challenges/name-of-employees/problem
+
+--Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
+
+SELECT name FROM employee
+ORDER BY name ASC;
