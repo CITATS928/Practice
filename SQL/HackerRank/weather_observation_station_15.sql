a7fcffd175339d4cb8d352c125595db946de0d00
CITATS928 <93238522+CITATS928@users.noreply.github.com>
2024-09-29 15:57:17 -0400

    weather obser 15

---
+++ SQL/HackerRank/weather_observation_station_15.sql
@@ -0,0 +1,8 @@
+--https://www.hackerrank.com/challenges/weather-observation-station-15/problem
+
+--Query the Western Longitude (LONG_W) for the largest Northern Latitude (LAT_N) in STATION that is less than . Round your answer to  decimal places.
+
+SELECT ROUND(LONG_W,4) FROM station
+WHERE LAT_N<137.2345
+ORDER BY LAT_N DESC
+LIMIT 1;
