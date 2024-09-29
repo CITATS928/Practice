f20781379f55e90173d6802948dcc92485d5437f
CITATS928 <93238522+CITATS928@users.noreply.github.com>
2024-09-29 16:03:47 -0400

    weather obser 16

---
+++ SQL/HackerRank/weather_observation_station_16.sql
@@ -0,0 +1,6 @@
+--https://www.hackerrank.com/challenges/weather-observation-station-16/problem
+
+--Query the smallest Northern Latitude (LAT_N) from STATION that is greater than 38.7780. Round your answer to 4 decimal places.
+
+SELECT ROUND(MIN(lat_n),4) FROM station
+WHERE lat_n>38.7780;
