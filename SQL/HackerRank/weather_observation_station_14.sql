52be9a5af65b15a429b8fcffa09cc6680a1bac80
CITATS928 <93238522+CITATS928@users.noreply.github.com>
2024-09-29 15:56:59 -0400

    fix&improve 14

--- SQL/HackerRank/weather_observation_station_14.sql
+++ SQL/HackerRank/weather_observation_station_14.sql
@@ -3,7 +3,14 @@
 --Query the greatest value of the Northern Latitudes (LAT_N) from STATION that is less than 137.2345. Truncate your answer to 4 decimal places.
 
 
-SELECT ROUND(lat_N,4) FROM station
+SELECT TRUNCATE(lat_N,4) FROM station
 WHERE lat_N<137.2345
 ORDER BY lat_N DESC
 LIMIT 1;
+
+/*
+Another way
+
+SELECT TURNCATE(MAX(lat_n),4) FROM station
+WHERE lat_N<137.2345;
+*/
