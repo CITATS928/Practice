481fe12db1a97d36f995ac5895606b0f27264f26
CITATS928 <93238522+CITATS928@users.noreply.github.com>
2024-10-02 14:41:34 -0400

    avg fun 2

---
+++ SQL/HackerRank/average_population.sql
@@ -0,0 +1,5 @@
+--https://www.hackerrank.com/challenges/average-population/problem
+
+--Query the average population for all cities in CITY, rounded down to the nearest integer.
+
+SELECT FLOOR(AVG(population)) FROM city;
