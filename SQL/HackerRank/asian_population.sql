e736b9211188110575a1356198247806ae65532e
CITATS928 <93238522+CITATS928@users.noreply.github.com>
2024-10-06 08:25:24 -0400

    population join

---
+++ SQL/HackerRank/asian_population.sql
@@ -0,0 +1,9 @@
+--https://www.hackerrank.com/challenges/asian-population/problem?isFullScreen=true
+
+-- Given the CITY and COUNTRY tables, query the sum of the populations of all cities where the CONTINENT is 'Asia'.Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+
+SELECT SUM(city.population)
+FROM city
+inner join country
+ON city.countrycode = country.code
+WHERE continent='Asia';
