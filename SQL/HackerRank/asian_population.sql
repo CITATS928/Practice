5fd8dd1d209c6383414992296fda0040d384182e
CITATS928 <93238522+CITATS928@users.noreply.github.com>
2024-10-06 08:27:45 -0400

    improve

--- SQL/HackerRank/asian_population.sql
+++ SQL/HackerRank/asian_population.sql
@@ -7,3 +7,19 @@ FROM city
 inner join country
 ON city.countrycode = country.code
 WHERE continent='Asia';
+
+
+/*
+Improved version
+
+使用别名提高可读性
+简化查询：表名可能较长，使用别名会让查询更加简洁。
+提高可读性：当涉及更多的列或多个表时，别名能够让代码更具可读性。
+
+SELECT SUM(c.population)
+FROM city c
+INNER JOIN country cn
+ON c.countrycode = cn.code
+WHERE cn.continent = 'Asia';
+
+*/
