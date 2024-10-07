9a30202869ce7cd0cfce4c36a49534f524c0a93c
CITATS928 <93238522+CITATS928@users.noreply.github.com>
2024-10-07 13:30:54 -0400

    africancity

---
+++ SQL/HackerRank/african_cities.sql
@@ -0,0 +1,8 @@
+--https://www.hackerrank.com/challenges/african-cities/problem?isFullScreen=true
+
+--Given the CITY and COUNTRY tables, query the names of all cities where the CONTINENT is 'Africa'.Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+
+SELECT city.name FROM city
+INNER JOIN country
+ON city.countrycode = country.code
+WHERE country.continent='Africa';
