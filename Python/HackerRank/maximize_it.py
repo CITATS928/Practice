0cfe518bf83118360b4ab9760dcda2089a6c00af
CITATS928 <93238522+CITATS928@users.noreply.github.com>
2024-10-12 22:08:56 -0400

    improved maximize it

--- Python/HackerRank/maximize_it.py
+++ Python/HackerRank/maximize_it.py
@@ -21,3 +21,26 @@ combination = itertools.product(*lists)
 for comb in combination:
     result.append(sum(x*x for x in comb)%M)
 print(max(result))
+
+"""
+improved version:
+
+import itertools
+import sys
+
+input_data = sys.stdin.read().split('\n')
+K, M = map(int,input_data[0].split())
+
+# Get the list of lists
+lists = [list(map(int, line[1:].split())) for line in input_data[1:]]
+
+max_result = 0
+
+# 计算所有组合，找到最大值
+for comb in itertools.product(*lists):
+    current_result = sum(x**2 for x in comb)%M
+    if current_result > max_result:
+        max_result = current_result
+
+print(max_result)
+"""
