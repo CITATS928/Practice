09145f33db518494eeefc90b10633d349e628550
CITATS928 <93238522+CITATS928@users.noreply.github.com>
2024-10-14 17:12:03 -0400

    wrap improved

--- Python/HackerRank/text_wrap.py
+++ Python/HackerRank/text_wrap.py
@@ -42,3 +42,14 @@ if __name__ == '__main__':
     string, max_width = input(), int(input())
     result = wrap(string, max_width)
     print(result)
+
+
+"""
+improved version:
+added join() method to join the list of strings into a single string
+
+def wrap(string, max_width):
+    chunks = [string[i:i+max_width] for i in range(0, len(string), max_width)]
+
+    return '\n'.join(chunks)
+"""
