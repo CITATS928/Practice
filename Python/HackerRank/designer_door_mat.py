0c486acdd0b772b27ff15aa12dda12b5f8a6ec74
CITATS928 <93238522+CITATS928@users.noreply.github.com>
2024-10-15 17:45:45 -0400

    door mat

---
+++ Python/HackerRank/designer_door_mat.py
@@ -0,0 +1,42 @@
+"""
+https://www.hackerrank.com/challenges/designer-door-mat/problem?isFullScreen=true
+
+Mr. Vincent works in a door mat manufacturing company. One day, he designed a new door mat with the following specifications:
+
+Mat size must be N X M. (N is an odd natural number, and M is 3 times N.)
+The design should have 'WELCOME' written in the center.
+The design pattern should only use |, . and - characters.
+
+Sample Input
+9 27
+
+Sample Output
+------------.|.------------
+---------.|..|..|.---------
+------.|..|..|..|..|.------
+---.|..|..|..|..|..|..|.---
+----------WELCOME----------
+---.|..|..|..|..|..|..|.---
+------.|..|..|..|..|.------
+---------.|..|..|.---------
+------------.|.------------
+"""
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import sys
+
+input_size = sys.stdin.read().split()
+N=int(input_size[0])
+M=int(input_size[1])
+
+design='.|.'
+pattern='---'
+
+for i in range(0, (N//2)):
+    print((pattern*((N//2)-i))+(design*(1+i*2))+(pattern*((N//2)-i)))
+    
+print(('-'*((M-7)//2))+'WELCOME'+('-'*((M-7)//2)))
+
+for i in range((N//2)-1, -1,-1):
+    print((pattern*((N//2)-i))+(design*(1+i*2))+(pattern*((N//2)-i)))
+    
