89b7862776361ae01a02a1be2e1f3183c9b44acf
CITATS928 <93238522+CITATS928@users.noreply.github.com>
2024-10-21 21:03:07 -0400

    improved rangoli

--- Python/HackerRank/alphabet_rangoli.py
+++ Python/HackerRank/alphabet_rangoli.py
@@ -59,3 +59,46 @@ def print_rangoli(size):
 if __name__ == '__main__':
     n = int(input())
     print_rangoli(n)
+
+
+"""
+关于最长宽度: 
+字母：size*2-1，连字符：size*2-1-1
+所以公式是：（size*2-1）+(size*2-1-1)=size*4-3
+
+improved version:
+def print_rangoli(size):
+    # 创建会用到的字母表（a-i)
+    alphabet = [chr(ord('a') + i) for i in range(size)]
+    # out put for size 5: ['a', 'b', 'c', 'd', 'e']
+
+    # Calculate the width (size is 5, width is 17)
+    width = size * 4 - 3
+
+    #rangoli lines
+    lines = []
+
+    # main loop
+    for i in range(size):
+        left_part = alphabet[size-1:i:-1]
+        # left part is ['e', 'd', 'c', 'b'] for i=0
+        right_part = alphabet[i:size]
+        # right part is ['a', 'b', 'c', 'd', 'e'] for i=0
+        full_line = left_part + right_part
+        # full line is ['e', 'd', 'c', 'b', 'a', 'b', 'c', 'd', 'e'] for i=0
+        line = '-'.join(full_line).center(width, '-')
+        # line is 'e-d-c-b-a-b-c-d-e' for i=0
+        lines.append(line)
+    # lines will be ['e-d-c-b-c-d-e', '----e-d-c-d-e----', '------e-d-e------', '--------e--------'] 
+
+    # print the upper half & middle line
+    for line in lines[::-1]:
+        print(line)
+    # print the lower half
+    for line in lines[1:]:
+        print(line)
+
+if __name__ == '__main__':
+    n = int(input())
+    print_rangoli(n)
+"""
