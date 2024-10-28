d059c83781e84264d880f83dda1fe9d86dd0704f
CITATS928 <93238522+CITATS928@users.noreply.github.com>
2024-10-27 21:42:53 -0400

    improved merge tools

--- Python/HackerRank/merge_the_tools.py
+++ Python/HackerRank/merge_the_tools.py
@@ -27,4 +27,17 @@ if __name__ == '__main__':
     string, k = input(), int(input())
     merge_the_tools(string, k)
 
+"""
+# improved version:
+减少for循环数量，使用dict保持substring中的字符的顺序并去重
 
+def merge_the_tools(string, k):
+    for i in range(0, len(string), k):
+        # 将string拆分成长度为k的substring
+        substring = string[i:i+k]
+
+        # 使用字典保持substring中的字符的顺序并去重
+        result = ''.join(dict.fromkeys(substring))
+
+        print(result)
+"""
