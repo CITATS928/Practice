e72fe2b32ae6115050038eaff6c08ef0067a346f
Qian Wang <93238522+CITATS928@users.noreply.github.com>
2024-11-21 01:44:08 +0000

    improved swap

--- Python/HackerRank/swap_c.py
+++ Python/HackerRank/swap_c.py
@@ -17,3 +17,7 @@ if __name__ == '__main__':
     s = input()
     result = swap_case(s)
     print(result)
+
+# improve function : 使用list生成式
+def swap_case(s):
+    return ''.join([char.upper() if char.islower() else char.lower() if char.isupper() else char for char in s])
