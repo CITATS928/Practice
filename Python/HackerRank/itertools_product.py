748c9ce433cb397997057b217291de75b543d5dd
Nightingale <93238522+CITATS928@users.noreply.github.com>
2024-11-25 22:32:16 -0500

    product imp

--- Python/HackerRank/itertools_product.py
+++ Python/HackerRank/itertools_product.py
@@ -15,3 +15,11 @@ for item in tuple(itertools.product(*l)):
     
 print(s)
 
+# 优化
+# 使用List Comprehension，一步完成数据处理并且为每一行的数据转换为list
+# 使用str.join() for String Concatenation
+
+input_data = [list(map(int, line.split()) for line in sys.stdin.read().splitlines())]
+
+result = ' '.join(str(item) for item in itertools.product(*input_data))
+print(result)
