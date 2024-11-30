ef2dcec5bce4632c555754a42a8349b104b24b56
Nightingale <93238522+CITATS928@users.noreply.github.com>
2024-11-29 22:16:36 -0500

    improve sym dif

--- Python/HackerRank/symmetric_diff.py
+++ Python/HackerRank/symmetric_diff.py
@@ -12,3 +12,8 @@ set_intersection = set_a.intersection(set_b)
 set_final = sorted(set_union.difference(set_intersection))
 for num in set_final:
     print(num)
+
+
+# 直接使用symmetric_difference()方法
+set_final = sorted(set_a.symmetric_difference(set_b))
+print('\n'.join(map(str, set_final)))
