bb0b38781aaa693de21af565cf98ece2aba7edb2
Nightingale <93238522+CITATS928@users.noreply.github.com>
2024-11-30 21:19:51 -0500

    note add

--- Python/HackerRank/set_add.py
+++ Python/HackerRank/set_add.py
@@ -12,3 +12,12 @@ for line in input_data[1:]:
     
 # print(len(s))
 sys.stdout.write(f'{len(s)}')
+
+
+######################
+
+# 注意事项：sys.stdout.write()方法不会自动换行，需要手动添加换行符
+sys.stdout.write(f'{len(s)}\n')
+
+# another way, 直接导入set，不需要循环
+s = set(input_data[1:])
