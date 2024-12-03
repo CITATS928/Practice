74cc599bf2978024938057548a0e98dcc58d40ca
Nightingale <93238522+CITATS928@users.noreply.github.com>
2024-12-02 21:57:55 -0500

    1418

---
+++ Python/LeetCode/1418.py
@@ -0,0 +1,33 @@
+# 1418. Display Table of Food Orders in a Restaurant
+# https://leetcode.com/problems/display-table-of-food-orders-in-a-restaurant/description/?envType=problem-list-v2&envId=azgp9mnm
+
+from collections import defaultdict
+class Solution:
+    def displayTable(self, orders: List[List[str]]) -> List[List[str]]:
+        set_table = set()
+        set_food = set()
+        for _, number, food in orders:
+            set_table.add(int(number))
+            set_food.add(food)
+        ordered_table = sorted(set_table)
+        ordered_food = sorted(set_food)
+        # print(ordered_food)
+        new_dict = {table:defaultdict(int) for table in ordered_table}
+
+        for _, table, item in orders:
+            new_dict[int(table)][item]+=1
+
+        # print(new_dict)
+
+        result = []
+        result.append(['Table'] + ordered_food)
+
+        for table in ordered_table:
+            row=[str(table)]
+            for food in ordered_food:
+                row.append(str(new_dict[table][food]))
+            result.append(row)
+
+        # print(result)
+        return result
+    
