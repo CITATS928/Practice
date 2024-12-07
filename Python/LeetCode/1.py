5693febd37c67a51aadd5cb827add9e06d249e3e
Nightingale <93238522+CITATS928@users.noreply.github.com>
2024-12-06 22:23:12 -0500

    1 imp

--- Python/LeetCode/1.py
+++ Python/LeetCode/1.py
@@ -9,3 +9,23 @@ class Solution:
                     return [i,j]
         return []
     
+# 使用一个字典来储存已经遍历过的数字，key是数字本身，value是数字的index。
+# 遍历nums寻找差值，如果target-nums[i]在字典中，返回两个数字的index。
+# 每次遍历的时候，达成以下目的：
+# 1. target - 当前数字 = 差值
+# 2. 这个差值之前有储存在字典中吗
+# 如果见过，直接返回它的index和当前数字的index
+# 如果没见过，把当前数字储存在字典中，等待下次遍历
+
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        dic = {}
+
+        for i in range(len(nums)):
+            if target-nums[i] in dic:
+                return [i, dic[target-nums[i]]] # 返回当前数字的index和之前数字的index
+            dic[nums[i]] = i  # 储存当前数字的index
+        
+        # 如果没有找到
+        return []
+    
