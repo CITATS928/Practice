707553b9aa015b4fa7dec27f27fb02a5a5104865
Nightingale <93238522+CITATS928@users.noreply.github.com>
2024-12-08 22:22:24 -0500

    set diff

--- Python/HackerRank/py_set_diff.py
+++ Python/HackerRank/py_set_diff.py
@@ -6,6 +6,6 @@ input_data = sys.stdin.read().splitlines()
 set_a = set(map(int, input_data[1].split()))
 set_b = set(map(int, input_data[3].split()))
 
-new_set = set_a.difference(set_b)
+new_set = set_a.difference(set_b) # same as set_a - set_b
 result = len(new_set)
 sys.stdout.write(str(result))
