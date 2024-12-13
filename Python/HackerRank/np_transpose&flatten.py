154d2449fb6e765c23afa8a1af7b9343438dc8c3
Nightingale <93238522+CITATS928@users.noreply.github.com>
2024-12-12 23:39:05 -0500

    imp tran&flatten, turn into array first

--- Python/HackerRank/np_transpose&flatten.py
+++ Python/HackerRank/np_transpose&flatten.py
@@ -3,8 +3,20 @@ import sys
 
 input_data = sys.stdin.read().splitlines()
 array = [list(map(int, data.split())) for data in input_data[1:]]
+
 transpose_result = numpy.transpose(array)
 sys.stdout.write(str(transpose_result)+'\n')
+
 np_array = numpy.array(array)
 flatten_result = np_array.flatten()
 sys.stdout.write(str(flatten_result))
+
+
+# 先转换为数组：
+array = numpy.array([list(map(int, data.split())) for data in input_data[1:]])
+
+# 转置：
+transpose_result = numpy.transpose(array)
+
+# 展平：
+flatten_result = transpose_result.flatten()
