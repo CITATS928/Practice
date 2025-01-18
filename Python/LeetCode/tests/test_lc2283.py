88d2fbee9a8126e924397e10b9a6e13b1fad89de
Nightingale <93238522+CITATS928@users.noreply.github.com>
2025-01-17 23:23:18 -0500

    unittest 2283

---
+++ Python/LeetCode/tests/test_lc2283.py
@@ -0,0 +1,29 @@
+import unittest
+from solutions.lc2283 import Solution
+
+class TestDigitCount(unittest.TestCase):
+    def setUp(self):
+        self.solution = Solution()
+
+    def test_example1(self):
+        num = "1210"
+        # expect True
+        self.assertEqual(self.solution.digitCount(num), True)
+
+    def test_example2(self):
+        num = "030"
+        # expect False
+        self.assertEqual(self.solution.digitCount(num), False)
+
+    def test_example3(self):
+        num = "1"
+        # expect False
+        self.assertEqual(self.solution.digitCount(num), False)
+
+    def test_example4(self):
+        num = "0000"
+        # expect True
+        self.assertEqual(self.solution.digitCount(num), True)
+
+if __name__ == '__main__':
+    unittest.main()
